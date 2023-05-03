a91a0ee22f14a14855627979d215aa6cf767d092
Andrey-Benicchio <andrey.bizerra@sptech.school>
2023-05-03 20:43:54 -0300

    criacao da classe treino e classe atividade

--- src/main/java/br/com/shapeup/backoffice/domain/Training.java
+++ src/main/java/br/com/shapeup/backoffice/domain/Training.java
@@ -1,20 +1,39 @@
 package br.com.shapeup.backoffice.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.sql.Time;
 import java.time.LocalDateTime;
+import java.util.List;
 
+@Entity
 public class Training {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
     private String name;
+    @NotNull
+    private String category;
+    @NotNull
+    private double Xp;
+    @OneToMany
+    private List<Atividade> atividades;
     private String description;
     private int duration;
 
+    public Long getId() {
+        return id;
+    }
 
-    public Training(String name, String description, int duration) {
-        this.name = name;
-        this.description = description;
-        this.duration = duration;
-
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -23,6 +42,30 @@ public class Training {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getXp() {
+        return Xp;
+    }
+
+    public void setXp(double xp) {
+        Xp = xp;
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(List<Atividade> atividades) {
+        this.atividades = atividades;
     }
 
     public String getDescription() {
@@ -40,8 +83,6 @@ public class Training {
     public void setDuration(int duration) {
         this.duration = duration;
     }
-
-
 }
 
 
