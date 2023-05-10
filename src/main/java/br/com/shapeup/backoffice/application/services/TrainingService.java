fd4555968901281ccad02a7fead66780e07765dc
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-10 03:11:32 -0300

    implementa classe de servico

--- src/main/java/br/com/shapeup/backoffice/application/services/TrainingService.java
+++ src/main/java/br/com/shapeup/backoffice/application/services/TrainingService.java
@@ -1,7 +1,41 @@
 package br.com.shapeup.backoffice.application.services;
 
+import br.com.shapeup.backoffice.application.mapper.TrainingMapper;
+import br.com.shapeup.backoffice.application.web.requests.TrainingRegisterRequest;
+import br.com.shapeup.backoffice.application.web.responses.TrainingRegistredResponse;
+import br.com.shapeup.backoffice.domain.Training;
+import br.com.shapeup.backoffice.repository.TrainingRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
+
+    @Autowired
+    TrainingRepository trainingRepository;
+
+    @Autowired
+    TrainingMapper trainingMapper;
+
+    public TrainingRegistredResponse saveTraining(TrainingRegisterRequest trainingRegisterRequest) {
+
+        Training training = trainingMapper.traningRegisterToTraining(trainingRegisterRequest);
+        trainingRepository.save(training);
+
+        return trainingMapper.trainingToTrainingRegistredResponse(training);
+    }
+
+    public List<Training> findAll() {
+
+        return trainingRepository.findAll();
+    }
+
+    public List<Training> findByName(String name) {
+        return trainingRepository.findByName(name);
+    }
+
+    public List<Training> findByCategoryAndDuration(String category, int duration) {
+        return trainingRepository.findByCategoryAndDuration(category, duration);
+    }
 }
