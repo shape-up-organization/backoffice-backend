83304872dd43f33face45df0909f99398d6b989b
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-05-10 03:12:39 -0300

    mapper

---
+++ src/main/java/br/com/shapeup/backoffice/application/mapper/TrainingMapper.java
@@ -0,0 +1,31 @@
+package br.com.shapeup.backoffice.application.mapper;
+
+import br.com.shapeup.backoffice.application.web.requests.TrainingRegisterRequest;
+import br.com.shapeup.backoffice.application.web.responses.TrainingRegistredResponse;
+import br.com.shapeup.backoffice.domain.Training;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrainingMapper {
+
+    public Training traningRegisterToTraining(TrainingRegisterRequest trainingRegisterRequest) {
+        return new Training(
+                trainingRegisterRequest.getName(),
+                trainingRegisterRequest.getCategory(),
+                trainingRegisterRequest.getXp(),
+                trainingRegisterRequest.getDescription(),
+                trainingRegisterRequest.getDuration()
+        );
+    }
+
+    public TrainingRegistredResponse trainingToTrainingRegistredResponse(Training training) {
+        return new TrainingRegistredResponse(
+                training.getName(),
+                training.getCategory(),
+                training.getXp(),
+                training.getDescription(),
+                training.getDuration()
+        );
+    }
+
+}
